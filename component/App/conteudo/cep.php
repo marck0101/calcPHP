96e44d8b395edeb647257d4c081267b4df7b1af2
marck0101 <marcos.henrique.a7x@gmail.com>
2022-09-05 16:51:50 -0300

    criado nova branch e estrutura dentro de components para fazer a calculadora

---
+++ component/App/conteudo/cep.php
@@ -0,0 +1,11 @@
+<div class="borda">
+    <div class="">
+        <h5>1.Local da instalação</h5>
+        <label>CEP:</label><br>
+        <input class="form-control" type="text" id="cep" name="cep" oninput="getCEP();"
+            value="" placeholder="CEP do local"  maxlength="8" required>
+        <br>
+        <label>Localidade:</label><br>
+        <div id="localidade"></div>
+    </div>
+</div>
---
+++ component/App/conteudo/gastoMensal.php
@@ -0,0 +1,8 @@
+<div class="borda">
+    <h5>4.Quanto gasta por mês?</h5>
+    <label>R$:</label><br>
+    <span class="input-euro left" type="text" id="gastoMensal" name="gastoMensal">
+        <input class="form-control " name="message" id="contaMes" type="text"
+            placeholder="2000" maxlength="8" value="2000" />
+    </span>
+</div>
