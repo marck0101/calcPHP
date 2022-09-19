ec38527caf04d160d9a9adfdcf5a48cd811fb9e2
Marcos <marcos.henrique.a7x@@gmail.com>
2022-09-19 09:34:47 -0300

    ajustado apontamento para as pastas

--- component/index.php
+++ component/index.php
@@ -5,10 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script language="javascript" type="text/javaScript" src="../component/js/calculos.js"></script>
-    <script language="javascript" type="text/javaScript" src="../component/js/Testejson.js"></script>
-    <link rel="stylesheet" type="text/css" href="../src/estilo/meuCss.css">
-    </link>
+    <script language="javascript" type="text/javaScript" src="./js/calculos.js"></script>
+    <script language="javascript" type="text/javaScript" src="./js/Testejson.js"></script>
+    <link rel="stylesheet" type="text/css" href="./css/meuCss.css">
+</link>
 
     <title>Document</title>
 </head>
