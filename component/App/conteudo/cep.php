7e414e9966184191f21a9fe2e3cb0de021b11974
marck0101 <marcos.henrique.a7x@gmail.com>
2022-09-26 15:08:00 -0300

    ajustes no media

--- component/App/conteudo/cep.php
+++ component/App/conteudo/cep.php
@@ -2,8 +2,7 @@
     <div class="">
         <h5>1.Local da instalação</h5>
         <label>CEP:</label><br>
-        <input class="form-control" type="text" id="cep" name="cep" oninput="getCEP();"
-            value="" placeholder="CEP do local"  maxlength="8" required>
+        <input class="form-control form-control-sm" type="text" id="cep" name="cep" oninput="getCEP();" value="" placeholder="CEP do local" maxlength="8" required>
         <br>
         <label>Localidade:</label><br>
         <div id="localidade"></div>
