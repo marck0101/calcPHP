a718318a93fffedc25373d180fae5455a8de4990
marck0101 <marcos.henrique.a7x@gmail.com>
2022-09-27 15:55:07 -0300

    deletado pasta test

--- tests/src/corpo/tipoInstalacao.php
+++
@@ -1,7 +0,0 @@
-<div class="borda">
-    <h5>2.Tipo de instalação</h5>
-    <select name="local" id="local" class="btn btn-danger" on-Change="combo(this, 'box)">
-        <option value="empresa" selected>Empresa</option>
-        <option value="outro">Outro</option>
-    </select>
-</div>
